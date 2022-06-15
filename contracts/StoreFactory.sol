96e5205b7274a12e35b8b48a2abad3e8ee6bc679
Vladimir Patsenko <v@patsenko.ru>
2022-06-15 18:01:46 +0300

    minimal proxy

--- contracts/StoreFactory.sol
+++ contracts/StoreFactory.sol
@@ -1,40 +1,27 @@
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
 contract StoreFactory {
     address public owner;
     address public impl;
 
     event cloneCreated(address cloned);
 
+    using Clones for address;
+
     modifier OnlyOwner() {
         require(msg.sender == owner, "Only the owner can clone a store");
         _;
     }
 
-    constructor(address _impl) public {
+    constructor(address _impl) {
         owner = msg.sender;
         impl = _impl;
     }
 
     function cloneStore() public OnlyOwner {
-        address ad = clone(impl);
+        address ad = impl.clone();
         emit cloneCreated(ad);
-    }
-
-    function clone(address implementation) internal returns (address instance) {
-        assembly {
-            let ptr := mload(0x40)
-            mstore(
-                ptr,
-                0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
-            )
-            mstore(add(ptr, 0x14), shl(0x60, implementation))
-            mstore(
-                add(ptr, 0x28),
-                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
-            )
-            instance := create(0, ptr, 0x37)
-        }
-        require(instance != address(0), "ERC1167: create failed");
     }
 }
