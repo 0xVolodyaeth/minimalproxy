e65525b9f2dafc8ba0be180da7d8875017303a96
Vladimir Patsenko <v@patsenko.ru>
2022-06-18 11:26:27 +0300

    minimal proxy and clones

--- contracts/StoreFactory.sol
+++ contracts/StoreFactory.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/IBeacon.sol";
 
-contract StoreFactory {
+contract StoreFactoryClones {
     address public owner;
     address public impl;
     event cloneCreated(address indexed cloned);
@@ -12,19 +15,83 @@ contract StoreFactory {
         _;
     }
 
-    constructor(address _master) {
+    constructor() {
         owner = msg.sender;
-        impl = _master;
+        impl = address(new StoreCl()); // master
     }
 
     function cloneStore(bytes32 _salt) public OnlyOwner {
         address ad = Clones.cloneDeterministic(impl, _salt);
-        address adEv = Clones.predictDeterministicAddress(impl, _salt);
-        emit cloneCreated(adEv);
+        emit cloneCreated(ad);
+    }
+}
+
+contract StoreCl {
+    string public value;
+
+    function setValue(string memory newValue) public {
+        value = newValue;
+    }
+}
+
+// contract StoreFactoryBeaconProxy {
+//     address public owner;
+//     address public impl;
+
+//     mapping(uint256 => address) public storeAddress;
+//     uint256 public id;
+
+//     UpgradeableBeacon immutable ub;
+
+//     modifier OnlyOwner() {
+//         require(msg.sender == owner, "Only the owner can clone a store");
+//         _;
+//     }
+
+//     constructor() {
+//         owner = msg.sender;
+//         impl = address(new StoreBe()); // master
+//         ub = new UpgradeableBeacon(impl);
+//     }
+
+//     function cloneStore(bytes memory _salt) public OnlyOwner {
+//         address proxyAddress = address(new BeaconProxy(address(ub), _salt));
+//         storeAddress[id] = proxyAddress;
+//         id++;
+//     }
+// }
+
+// contract StoreBe {
+//     string public value;
+
+//     function setValue(string memory newValue) public {
+//         value = newValue;
+//     }
+// }
+
+contract StoreFactory {
+    address public impl;
+    uint256 public id;
+
+    UpgradeableBeacon public ub;
+    mapping(uint256 => address) public storeAddress;
+
+    constructor(address _impl) {
+        impl = _impl;
+        ub = new UpgradeableBeacon(_impl);
     }
 
-    function getCloneAddress(bytes32 _salt) public view returns (address) {
-        address ad = Clones.predictDeterministicAddress(impl, _salt);
-        return ad;
+    function clone(bytes memory _salt) public returns (address) {
+        address proxyAddress = address(new BeaconProxy(address(ub), _salt));
+        storeAddress[id] = proxyAddress;
+        id++;
+    }
+}
+
+contract StoreMaster {
+    string public value;
+
+    function initialize(string memory _val) public {
+        value = _val;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,46 @@
-import { isCommunityResourcable } from "@ethersproject/providers";
-import { expect } from "chai";
-import { sign } from "crypto";
+import { resolve, Resolver } from "dns";
+import { ContractTransaction } from "ethers";
 import { ethers } from "hardhat";
-import { emit } from "process";
 
-describe("Greeter", function () {
+describe("StoreProxy", function () {
 
-  it("Should return the new greeting once it's changed", async function () {
-    const Store = await ethers.getContractFactory("Store");
+  const getGasUsed = async (tx: ContractTransaction): Promise<number> => {
+    let reciept = await tx.wait()
+    return Promise.resolve(reciept.gasUsed.toNumber());
+  }
+
+  it("Should print amount of gas used to deploy a clone", async function () {
+    const Store = await ethers.getContractFactory("StoreCl");
+    const StoreFactory = await ethers.getContractFactory("StoreFactoryClones");
+
+    const sf = await StoreFactory.deploy();
+    await sf.deployed();
+
+    let eventFilter = sf.filters.cloneCreated()
+
+    const salt = ethers.utils.formatBytes32String('1');
+    const cloneTx = await sf.cloneStore(salt);
+
+    let events = await sf.queryFilter(eventFilter);
+    const store = Store.attach(events[0].args.cloned);
+    await store.setValue("first store val");
+
+    // second clone creation
+    const secondCloneSalt = ethers.utils.formatBytes32String('2');
+    const cloneTx2 = await sf.cloneStore(secondCloneSalt);
+    await cloneTx2.wait();
+
+    let eventsFromSecondClone = await sf.queryFilter(eventFilter);
+
+    const secondStoreClone = Store.attach(eventsFromSecondClone[1].args.cloned);
+    await secondStoreClone.setValue("second store val");
+
+    let gasUsedToCreateClone = await getGasUsed(cloneTx2);
+    console.log(gasUsedToCreateClone);
+  });
+
+  it("Should print amount of gas used to deploy a clone", async function () {
+    const Store = await ethers.getContractFactory("StoreMaster");
     const StoreFactory = await ethers.getContractFactory("StoreFactory");
 
     const storeMaster = await Store.deploy();
@@ -16,35 +49,23 @@ describe("Greeter", function () {
     const sf = await StoreFactory.deploy(storeMaster.address);
     await sf.deployed();
 
-    let eventFilter = sf.filters.cloneCreated()
+    let tx = await sf.clone([]);
+    let gasUsed = await getGasUsed(tx);
+    console.log(gasUsed);
 
-    const impl = await sf.impl();
-    console.log(`implementation address: ${impl}`);
+    const proxyAddress = await sf.storeAddress(0);
+    const storeProxy = await Store.attach(proxyAddress);
 
-    const salt = ethers.utils.formatBytes32String('1');
-    const cloneTx = await sf.cloneStore(salt);
-    await cloneTx.wait();
+    tx = await storeProxy.initialize("proxy");
+    await tx.wait();
 
-    let events = await sf.queryFilter(eventFilter);
-    console.log("first  clone creation event: ", events, "\n");
+    tx = await storeMaster.initialize("master");
+    await tx.wait();
 
-    const store = Store.attach(events[0].args.cloned);
-    await store.setValue("message");
+    const proxyVal = await storeProxy.value();
+    console.log(proxyVal)
 
-
-
-    console.log("\n\n\n\n");
-
-    // second clone creation
-    const secondCloneSalt = ethers.utils.formatBytes32String('2');
-    const cloneTx2 = await sf.cloneStore(secondCloneSalt);
-    await cloneTx2.wait();
-
-    let eventsFromSecondClone = await sf.queryFilter(eventFilter);
-    console.log("second clone creation event: ", eventsFromSecondClone, "\n");
-
-    const secondStoreClone = Store.attach(eventsFromSecondClone[1].args.cloned);
-    await secondStoreClone.setValue("message");
-  });
-
+    const masterVal = await storeMaster.value();
+    console.log(masterVal)
+  })
 });
